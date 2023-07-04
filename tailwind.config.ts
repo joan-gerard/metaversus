fc26e92236ef928ff7ee1a365ae5e238b5e29221
Joan Gerard <joan.gerard@outlook.com>
2023-07-04 13:20:10 +0200

    add tailwind

---
+++ tailwind.config.ts
@@ -0,0 +1,17 @@
+import type { Config } from 'tailwindcss'
+
+export default {
+  content: ['./app/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        'primary-black': '#1A232E',
+        'secondary-white': '#c7c7c7',
+      },
+      transitionTimingFunction: {
+        'out-flex': 'cubic-bezier(0.05, 0.6, 0.4, 0.9)',
+      },
+    },
+  },
+  plugins: [],
+} satisfies Config
