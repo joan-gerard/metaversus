1e807f8b10c20a5ae24ce797d39e940dd598595a
Joan Gerard <joan.gerard@outlook.com>
2023-07-04 13:43:12 +0200

    create empty organisms

---
+++ app/organisms/index.ts
@@ -0,0 +1,23 @@
+import Navbar from './Navbar';
+import Hero from './Hero';
+import About from './About';
+import Explore from './Explore';
+import GetStarted from './GetStarted';
+import WhatsNew from './WhatsNew';
+import World from './World';
+import Insights from './Insights';
+import Feedback from './Feedback';
+import Footer from './Footer';
+
+export {
+  Navbar,
+  Hero,
+  About,
+  Explore,
+  GetStarted,
+  WhatsNew,
+  World,
+  Insights,
+  Feedback,
+  Footer
+};
